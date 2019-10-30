753883980629c6a9027c188f07f6a3e793382bed
yyj4399 <admin@yyj4399.com>
2019-10-30 17:18:57 +0800

    0

--- src/App.js
+++ src/App.js
@@ -4,31 +4,12 @@ import Menu from './menu';
 import 'weui';
 import 'react-weui/build/packages/react-weui.css';
 
-const axios = require('axios');
-
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       data: {},
     }
-  }
-  componentDidMount() {
-    axios.get('http://localhost:3001/')
-      .then(response => {
-        // handle success
-        console.log(response);
-        this.setState({data: response.data});
-      })
-      .catch(error => {
-        // handle error
-        console.log(error);
-      })
-      .then(() => {
-        // always executed
-        // console.log(this.reqdata)
-        console.log(this.state.data)
-      });
   }
   render() {
     return (
